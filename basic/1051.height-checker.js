5ab715f0a9905d25e23545f97a6c3ad331f3bcfe
tjdl102306 <tjdl102306@gmail.com>
2019-07-09 23:08:14 +0800

    tree

---
+++ basic/1051.height-checker.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=1051 lang=javascript
+ *
+ * [1051] Height Checker
+ */
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    
+};
+
