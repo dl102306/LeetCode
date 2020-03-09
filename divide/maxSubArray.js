c28791d4cef4c5491160660c9e58e58f97fffb4a
tjdl102306 <tjdl102306@gmail.com>
2020-03-09 21:52:54 +0800

    [53]  Maximum Subarray - divide

--- divide/maxSubArray.js
+++ divide/maxSubArray.js
@@ -29,3 +29,45 @@ var maxSubArray = function(nums) {
 
   return result;
 };
+
+var findMax = function(low, high, mid, arr) {
+    let max_v1 = arr[mid]
+    let result = arr[mid]
+    for (let i = (mid - 1); i >= low; i--) {
+        result = result + arr[i]
+
+        if (result > max_v1) {
+            max_v1 = result
+        }
+    }
+
+    let max_v2 = arr[mid + 1]
+    result = arr[mid + 1]
+    for (let i = (mid + 2); i <= high; i++) {
+        result = result + arr[i]
+        if (result > max_v2) {
+            max_v2 = result
+        }
+    }
+
+    return (max_v1 + max_v2)
+}
+
+var maxSubArray_v2 = function(low, high, arr) {
+    if (low === high) {
+        return arr[low]
+    }
+
+    const mid = Math.floor((low + high) / 2)
+    const v1 = maxSubArray_v2(low, mid, arr)
+    const v2 = maxSubArray_v2(mid + 1, high, arr)
+    const v3 = findMax(low, high, mid, arr)
+
+    return Math.max(v1, v2, v3)
+}
+
+function maxArr(arr) {
+    return maxSubArray_v2(0, arr.length - 1, arr)
+}
+
+console.log(maxArr([-2,1]))
