82398fc51af723a51266d1ab329e15c557dcec43
tjdl102306 <tjdl102306@gmail.com>
2020-05-21 22:06:59 +0800

    1373. Maximum Sum BST in Binary Tree

--- tree/maxSumBST.js
+++ tree/maxSumBST.js
@@ -10,83 +10,99 @@
     this.val = (val===undefined ? 0 : val)
     this.left = (left===undefined ? null : left)
     this.right = (right===undefined ? null : right)
-  }
+ }
 /**
  * @param {TreeNode} root
  * @return {number}
  */
 var maxSumBST = function(root) {
-  let arr = []
   let max = 0
-  let sum = 0
-  let flag = false
-
-  function midSort(node)  {
-    if (!node) {
-      return
-    }
-
-    midSort(node.left)
-    if (!flag) {
-      sum = sum + node.val
-      if (arr.length > 0 && (node.val <= arr[arr.length - 1])) {
-        sum = -1
-        flag = true
+  function getSum(node) {
+    if (!node.left && !node.right) {
+      const res = {
+        min: node.val,
+        max: node.val,
+        sum: node.val,
+        valid: true
       }
-      arr.push(node.val)
-      midSort(node.right)
+      // console.log(res)
+      if (node.val > max) {
+        max = node.val
+      }
+      return res
     }
-  }
 
-  const list = [root]
-  let header = 0
-  let tail = 0
+    let leftResult = null
+    let rightResult = null
 
-  while (header <= tail) {
-    const exe_node = list[header]
-    arr = []
-    sum = 0
-    flag = false
-    midSort(exe_node)
-    // console.log(arr)
-    // console.log(sum)
-    if (sum > max && !flag) {
+    if (node.left) {
+      leftResult = getSum(node.left)
+    }
+    if (node.right) {
+      rightResult = getSum(node.right)
+    }
+
+    if (leftResult && !leftResult.valid) {
+      return {
+        valid:false
+      }
+    }
+    if (rightResult && !rightResult.valid) {
+      return {
+        valid:false
+      }
+    }
+
+    if (leftResult && node.val <= leftResult.max) {
+      return {
+        valid: false
+      }
+    }
+
+    if (rightResult && node.val >= rightResult.min) {
+      return {
+        valid: false
+      }
+    }
+
+    const sum = node.val + (leftResult && leftResult.sum || 0) + (rightResult && rightResult.sum || 0)
+    if (sum > max) {
       max = sum
     }
-    if (exe_node.left) {
-      list.push(exe_node.left)
-      tail++
+
+    const res = {
+      valid: true,
+      min: (leftResult && leftResult.min || node.val),
+      max: (rightResult && rightResult.max || node.val),
+      sum: sum
     }
-    if (exe_node.right) {
-      list.push(exe_node.right)
-      tail++
-    }
-    header++
+
+    // console.log(res)
+    return res
   }
 
-  if (max === 11 && root.val === 8) {
-    return 18
-  }
+  getSum(root)
 
   return max
+
 };
 
-// const node_1 = new TreeNode(3)
-// const node_2 = new TreeNode(2)
-// const node_3 = new TreeNode(5)
-// const node_4 = new TreeNode(4)
-// const node_5 = new TreeNode(6)
-// node_1.left = node_2
-// node_1.right = node_3
-// node_3.left = node_4
-// node_3.right = node_5
-
-const node_1 = new TreeNode(4)
-const node_2 = new TreeNode(3)
-const node_3 = new TreeNode(1)
-const node_4 = new TreeNode(2)
+const node_1 = new TreeNode(3)
+const node_2 = new TreeNode(2)
+const node_3 = new TreeNode(5)
+const node_4 = new TreeNode(4)
+const node_5 = new TreeNode(6)
 node_1.left = node_2
-node_2.left = node_3
-node_2.right = node_4
+node_1.right = node_3
+node_3.left = node_4
+node_3.right = node_5
+
+// const node_1 = new TreeNode(4)
+// const node_2 = new TreeNode(3)
+// const node_3 = new TreeNode(1)
+// const node_4 = new TreeNode(2)
+// node_1.left = node_2
+// node_2.left = node_3
+// node_2.right = node_4
 
 console.log(maxSumBST(node_1))
